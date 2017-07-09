2776ec45b0f9ea741c15e3464c57cb51a5c7c412
uriel <urielrozenberg@hotmail.com>
2017-07-08 22:06:07 -0300

    median calculation from a slice of ints in O(n)

---
+++ mediana.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	// "net/http"
+	// "reflect"
+	//  "os"
+)
+func selection(list []int, k int)int{
+	n:=len(list)
+	for i:=0;i<=k;i++{
+		minIndex := i
+		minValue := list[i]
+		for j:=i;j< n;j++{
+			if list[j] < minValue{
+				minIndex = j
+				minValue = list[j]
+				list[i] ,list[minIndex]=list[minIndex],list[i]
+			}
+		}
+	}
+	return list[k]
+}
+func main(){
+	list:=[]int{2, 2,3,4,45,62,123,4,435,45,7,56345,2345,202,35446,65434534,45,2,64,332,433, 5, 7, 11, 13,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,}
+	//list :=[]int{1,2,3,4,5}
+	fmt.Println(selection(list,4))
+	fmt.Println(list)
+}
