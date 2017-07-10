4d99276b756e26a0652a59c66f94d0190eba4b39
uriel <urielrozenberg@hotmail.com>
2017-07-10 12:25:34 -0300

    moved median test to test file

--- mediana.go
+++ mediana.go
@@ -1,13 +1,6 @@
 package main
 
-import (
-	"fmt"
-	// "net/http"
-	// "reflect"
-	//  "os"
-)
-
-func selection(list []int, k int) int {
+func Selection(list []int, k int) int {
 	n := len(list)
 	for i := 0; i <= k; i++ {
 		minIndex := i
@@ -22,9 +15,6 @@ func selection(list []int, k int) int {
 	}
 	return list[k]
 }
-func mainnn() {
-	list := []int{2, 2, 3, 4, 45, 62, 123, 4, 435, 45, 7, 56345, 2345, 202, 35446, 65434534, 45, 2, 64, 332, 433, 5, 7, 11, 13, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
-	//list :=[]int{1,2,3,4,5}
-	fmt.Println(selection(list, 4))
-	fmt.Println(list)
+func Median(list []int) int {
+	return Selection(list, (len(list)/2)-1)
 }
