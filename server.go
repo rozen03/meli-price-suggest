9cea04e7e924fd4bf82e7388f0ddaa1aaeb85a72
uriel <urielrozenberg@hotmail.com>
2017-07-13 10:31:32 -0300

    postr 8081

--- server.go
+++ server.go
@@ -21,5 +21,5 @@ func start() {
 	r := gin.Default()
 	r.GET("/ping", ping)
 	r.GET("/categories/:id/prices", prices)
-	r.Run(":80") // listen and server on 0.0.0.0:80
+	r.Run(":8081") // listen and server on 0.0.0.0:8081
 }
