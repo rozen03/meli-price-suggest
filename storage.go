685921ce9dc3b0c72095fe5ef4fca33ead9d01ac
uriel <urielrozenberg@hotmail.com>
2017-07-13 13:12:36 -0300

    40 channels

--- storage.go
+++ storage.go
@@ -139,7 +139,7 @@ func PreciosYVentas(category string) obtainedData {
 	// fmt.Println(total, reflect.TypeOf(total))
 	res := GetPreciosYVentas(results)
 	resp.Body.Close()
-	chanels := 20
+	chanels := 40
 	for i := 200; i < total; i += 200 * (chanels) {
 		channs := make([]chan obtainedData, chanels)
 		for c := range channs {
