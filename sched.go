9f86516b9dd0e5ac1cf104f50bfa87d4065bf901
uriel <urielrozenberg@hotmail.com>
2017-07-19 22:32:15 -0300

    8k workers

--- sched.go
+++ sched.go
@@ -8,7 +8,7 @@ type ArgsAndResult struct {
 	get  httpInterface
 }
 
-const maxChanelsSched = 64
+const maxChanelsSched = 8000
 
 /*
 **A Task Worker is a Goroutine that is listening to a channel waiting for
